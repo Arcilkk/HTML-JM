22a5878d38e3deec39972d73eb21e78498fac993
Arcilkk <arcilaballenalejandro@gmail.com>
2026-06-18 11:47:45 -0500

    Add files via upload

---
+++ JM html/condicionales.js
@@ -0,0 +1,65 @@
+// let nombre, edad;
+// nombre = prompt("Ingrese su nombre ...");
+// edad = prompt('Ingrese su edad');
+// if (edad >= 18) {
+//     document.write('BIENVENIDO ', nombre);
+// } else {
+//     if (edad < 18) {
+//         document.write(nombre, ' es menor de edad');
+//     }
+//     else {
+//         document.write('NO HA INGRESADO DATOS');
+//     }
+// }
+//Dado un numero entre 1 y 5, determine el dia laboral que corresponde
+// let dia;
+// dia = prompt('Ingrese un numero entre 1 y 5');
+// if (dia == 1) {
+//     document.write('LUNES');
+// }else if (dia == 2) {
+//         document.write('MARTES');
+//     } else if (dia == 3) {
+//             document.write('MIERCOLES');
+//         } else if (dia == 4) {
+//                 document.write('JUEVES');
+//             } else if (dia == 5) {
+//                     document.write('VIERNES');
+//                 } else {
+//                     document.write('NO HA INGRESADO UN NUMERO ENTRE 1 Y 5');
+//                 }
+// UTILIZANDO SWITCH
+// let dia;
+// dia = parseInt(prompt('Ingrese un numero entre 1 y 5'));      
+// switch (dia) {
+//     case 1:
+//         document.write('LUNES'); break;
+//     case 2:
+//         document.write('MARTES'); break;
+//     case 3:
+//         document.write('MIERCOLES'); break;
+//     case 4:
+//         document.write('JUEVES'); break;
+//     case 5:
+//         document.write('VIERNES'); break;
+//     default:        document.write('NO HA INGRESADO UN NUMERO ENTRE 1 Y 5'); break;
+// }
+// CONDICIONALES CON EL OPERADOR LOGICO &&
+// let notaPractica, notaTeorica;
+// notaPractica = prompt('Ingrese la nota práctica');
+// notaTeorica = prompt('Ingrese la nota teórica');
+// if (notaPractica >= 4 && notaTeorica >=4) {
+//     notaDef = notaTeorica * 0.65 + notaPractica * 0.35;
+// } else {
+//     notaDef = 0;
+// }
+// document.write('La nota definitiva es: ' + notaDef);
+// let ventas, cant, comision;
+// ventas = prompt('Ingrese el valor total de las ventas');
+// cant = prompt('Ingrese la cantidad de ventas realizadas');
+// if (ventas > 5000000000 || cant > 100) {
+//     comision = ventas * 0.025;
+// }
+// else { comision = 0; }
+// document.write('La comisión es: ' + comision);   
+// Conversión de Longitud
+
